0b69b4abc2e6dcba5cda8d3d26f3c9570215f070
Zeno Zeng <zenoofzeng@gmail.com>
2014-03-11 21:24:17 +0800

    add macro unless (using list)

--- clojure/unless.clj
+++ clojure/unless.clj
@@ -1,4 +1,12 @@
+;; 关于 macro，可以看看这篇
+;; http://blog.csdn.net/ithomer/article/details/17590193
+
 (defmacro unless [test ifnil iftrue]
   `(if ~test ~iftrue ~ifnil))
+
+;; List 实现
+
+(defmacro unless [test ifnil iftrue]
+  (list 'if test iftrue ifnil))
 
 (unless false (println "TRUE") (println "FALSE"))
